fd41591163a4b99651fe42d7d710d62e2562edd3
Zoe Shaw <1141195807@qq.com>
2017-05-17 20:03:12 +0800

    fix bugs

--- lab4.8 Puzzle/Board.java
+++ lab4.8 Puzzle/Board.java
@@ -2,35 +2,51 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.MinPQ;
+import java.util.Iterator;
+
+import java.util.List;
+import java.util.LinkedList;
 
 public class Board {
     private int[][] board;
     private int n;
-    public Board(int[][] blocks) {
+    private int blankRow;
+    private int blankCol;
+
+    public Board(int[][] blocks) {// construct a board from an n-by-n array of blocks
       int n = block.size();
       for (int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
           board[i][j] = blocks[i][j];
         }
       }
+      blankRow = -1;
+      blankCol = -1;
+      for (int i = 0; i < n; i ++)
+        for (int j = 0; j < n; j++)
+          if (board[i][j] == 0) {
+            blankRow = i;
+            blankCol = j;
+          }
       n = board.size();
-    }           // construct a board from an n-by-n array of blocks
-                                           // (where blocks[i][j] = block in row i, column j)
-    public int dimension() {
+    }
+
+    public int dimension() {// board dimension n
       return board.size();
-    }                // board dimension n
+    }
     public int hamming() {
       int cnt = 0;
       int num = 1;
       for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-          if (board[i][j] != num) cnt++;
+          if (board[i][j] != num) cnt++;// number of blocks out of place
           num++;
         }
       }
       return cnt;
-    }                 // number of blocks out of place
-    public int manhattan() {
+    }
+    public int manhattan() {// sum of Manhattan distances between blocks and goal
       int cnt = 0;
       for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -55,14 +71,14 @@ public class Board {
         }
       }
       return cnt;
-    }               // sum of Manhattan distances between blocks and goal
-    public boolean isGoal() {
+    }
+    public boolean isGoal() {// is this board the goal board?
       Board goal;
       int arr = {{1,2,3},{4,5,6},{7,8}};
       goal.Board(arr);
       return goal.equals(board);
-    }               // is this board the goal board?
-    public Board twin() {
+    }
+    public Board twin() {// a board that is obtained by exchanging any pair of blocks
       while (true) {
         int x1 = Random(0, 2);
         int y1 = Random(0, 2);
@@ -76,8 +92,8 @@ public class Board {
       board[x1][y1] = board[x2][y2];
       board[x2][y2] = tmp;
       return board;
-    }                   // a board that is obtained by exchanging any pair of blocks
-    public boolean equals(Object y) {
+    }
+    public boolean equals(Object y) {// does this board equal y?
       for (int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
           if (y[i][j] != board[i][j]) {
@@ -86,11 +102,19 @@ public class Board {
         }
       }
       return true;
-    }       // does this board equal y?
-    public Iterable<Board> neighbors() {
-
-    }    // all neighboring boards
-    public String toString()               // string representation of this board (in the output format specified below)
+    }
+    public Iterable<Board> neighbors() {// all neighboring boards
+      List<Board> neighbors = new LinkedList<Board>();
+      
+    }
+    public String toString() {// string representation of this board (in the output format specified below)
+      String out = Interger.toString(dimension());
+      for (int i = 0; i < dimension(); i++)
+        for (int j = 0; j < dimension(); j++)
+          out += " " + board[i][j];
+        out += "\n";
+      return out;
+    }
 
     public static void main(String[] args) // unit tests (not graded)
 }
